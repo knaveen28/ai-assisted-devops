7f14be21b1ce07c604243da9f27f17ae475821f2
knaveen028 <naveeninfo.java@gmail.com>
2026-01-23 12:11:34 +0530

    update local python file

--- local-llms-ollama/generate_dockerfile.py
+++ local-llms-ollama/generate_dockerfile.py
@@ -1,5 +1,7 @@
 import ollama
 
+# preparing a prompt for generating a Dockerfile for a given programming language
+
 PROMPT = """
 ONLY Generate an ideal Dockerfile for language. Do not provide any description
 Include:
@@ -10,9 +12,13 @@ Include:
 - Running the application
 """
 
+# write a function to generate the Dockerfile using the Ollama API
+
 def generate_dockerfile(language):
     response = ollama.chat(model='llama3.1:8b', messages=[{'role': 'user', 'content': PROMPT.format(language=language)}])
     return response['message']['content']
+
+# invoke the function and print the result
 
 if __name__ == '__main__':
     language = input("Enter the programming language: ")
